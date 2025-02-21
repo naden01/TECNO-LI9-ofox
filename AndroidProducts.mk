c30dca9f9666b65b19cdf8a3a145e43573311b5f
CHIRO <90218247+shahqufi@users.noreply.github.com>
2025-02-21 22:29:07 +0530

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_TECNO-LI9.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_TECNO-LI9-user \
+    twrp_TECNO-LI9-userdebug \
+    twrp_TECNO-LI9-eng
