a863e84b25c62298618dbe322b246be62cdafdc9
CHIRO <90218247+shahqufi@users.noreply.github.com>
2025-02-26 02:36:18 +0530

    tyr

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,13 +150,13 @@ TARGET_SCREEN_DENSITY := 480
 TW_EXTRA_LANGUAGES            := true
 TARGET_USES_MKE2FS            := true
 
-TW_FRAMERATE          := 90
+TW_FRAMERATE          := 60
 TW_BRIGHTNESS_PATH    := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS     := 255
-TW_DEFAULT_BRIGHTNESS := 100
+TW_DEFAULT_BRIGHTNESS := 90
 TW_NO_SCREEN_BLANK    := true
 TARGET_LD_SHIM_LIBS := libminuitwrp
-TW_NO_CPU_TEMP := true
+TW_CUSTOM_CPU_TEMP_PATH := true
 
 
 # Tools
